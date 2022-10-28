8c485f14a81116500030ee246c4b0f6e3a056b5e
Alberto M <alberto@711media.tv>
2022-10-27 23:45:28 -0700

    initial commit. all empire logos and text added.

---
+++ src/assets/fake-data/dataProject.js
@@ -0,0 +1,140 @@
+import img1 from '../images/common/img1.jpg'
+import img2 from '../images/common/img2.jpg'
+import img3 from '../images/common/img3.jpg'
+import img4 from '../images/common/img4.jpg'
+import img5 from '../images/common/img5.jpg'
+import img6 from '../images/common/img6.jpg'
+import img7 from '../images/common/img7.jpg'
+import img9 from '../images/common/img9.jpg'
+import img10 from '../images/common/img10.jpg'
+import img11 from '../images/common/img11.jpg'
+import img12 from '../images/common/img12.jpg'
+import img13 from '../images/common/img13.jpg'
+import img14 from '../images/common/img14.jpg'
+
+
+const dataProject = [
+    {
+        id: 1,
+        title: 'EMPIRE NFT #01',
+        img: img1,
+    },
+    {
+        id: 2,
+        title: 'EMPIRE NFT #02',
+        img: img2,
+    },
+    {
+        id: 3,
+        title: 'EMPIRE NFT #03',
+        img: img3,
+    },
+    {
+        id: 4,
+        title: 'EMPIRE NFT #04',
+        img: img4,
+    },
+    {
+        id: 5,
+        title: 'EMPIRE NFT #05',
+        img: img5,
+    },
+    {
+        id: 6,
+        title: 'EMPIRE NFT #06',
+        img: img6,
+    },
+    {
+        id: 7,
+        title: 'EMPIRE NFT #07',
+        img: img7,
+    },
+    {
+        id: 8,
+        title: 'EMPIRE NFT #08',
+        img: img2,
+    },
+
+    {
+        id: 9,
+        title: 'EMPIRE NFT #09',
+        img: img9,
+    },
+    {
+        id: 10,
+        title: 'EMPIRE NFT #10',
+        img: img10,
+    },
+    {
+        id: 11,
+        title: 'EMPIRE NFT #11',
+        img: img11,
+    },
+    {
+        id: 12,
+        title: 'EMPIRE NFT #12',
+        img: img12,
+    },
+    {
+        id: 13,
+        title: 'EMPIRE NFT #13',
+        img: img13,
+    },
+    {
+        id: 14,
+        title: 'EMPIRE NFT #14',
+        img: img14,
+    },
+    {
+        id: 15,
+        title: 'EMPIRE NFT #15',
+        img: img1,
+    },
+    {
+        id: 16,
+        title: 'EMPIRE NFT #16',
+        img: img3,
+    },
+    {
+        id: 17,
+        title: 'EMPIRE NFT #01',
+        img: img1,
+    },
+    {
+        id: 18,
+        title: 'EMPIRE NFT #02',
+        img: img2,
+    },
+    {
+        id: 19,
+        title: 'EMPIRE NFT #03',
+        img: img3,
+    },
+    {
+        id: 20,
+        title: 'EMPIRE NFT #04',
+        img: img4,
+    },
+    {
+        id: 21,
+        title: 'EMPIRE NFT #05',
+        img: img5,
+    },
+    {
+        id: 22,
+        title: 'EMPIRE NFT #06',
+        img: img6,
+    },
+    {
+        id: 23,
+        title: 'EMPIRE NFT #07',
+        img: img7,
+    },
+    {
+        id: 24,
+        title: 'EMPIRE NFT #08',
+        img: img2,
+    },
+]
+
+export default dataProject;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import './App.scss'
+import ScrollToTop from "./ScrollToTop";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+
+const desiredChainId = ChainId.Mainnet;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <ThirdwebProvider desiredChainId={desiredChainId}>
+    <React.Fragment>
+        <BrowserRouter>
+        <ScrollToTop />
+            <App />
+        </BrowserRouter>
+    </React.Fragment>
+    </ThirdwebProvider>
+);
+
