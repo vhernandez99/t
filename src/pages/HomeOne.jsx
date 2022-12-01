dd790ff4eab15396da2dbd00e844435de513025b
Victor hernandez <vhernandez.99@hotmail.com>
2022-12-01 16:17:23 -0600

    add new pepes mundial panel

--- src/pages/HomeOne.jsx
+++ src/pages/HomeOne.jsx
@@ -41,7 +41,13 @@ function HomeOne(props) {
               async (contract) => contract.claimTo(address, 1)
             }
             > {translate("mintnow")} </Web3Button> */}
-
+ <iframe
+             src="https://gateway.ipfscdn.io/ipfs/Qmcine1gpZUbQ73nk7ZGCcjKBVFYXrEtqrhujXk3HDQ6Nn/erc721.html?contract=0x72cCB5A5b434d54084012b59191556D3EfBd0670&chainId=1"
+              width="100%"
+              height="700px"
+              className="max-width:100%;"
+              frameBorder="0"
+            ></iframe>
             <iframe
               src="https://gateway.ipfscdn.io/ipfs/QmPaVYdGue8zEXFKqrtVHpvzBvufM1DYzw5n1of3KVPG88/signature-drop.html?contract=0x0046AC5AD2De7d5c9aeBeCf67751cB7e0865a0EB&chainId=1&theme=dark&primaryColor=cyan"
               width="100%"
